e6604e2e5cb4c2c5225b1c1e974159dcb6bf3a0c
Randy Suarez <rsuarez.dev@gmail.com>
2022-09-07 23:15:41 -0500

    change some functions and Shortcuts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -167,6 +167,14 @@
 		},
 		created() {
 			Loading.show()
+			window.addEventListener('keypress', (event) => {
+				if (event.keyCode === 13) {
+					// key code of the keybord key
+					this.save()
+					// your code to Run
+				}
+			})
+
 			this.license()
 			let serial = 'randy'
 			var rsValue = (lines, property, separator, trimmed, lineMatch) => {
@@ -409,6 +417,28 @@
 			}, 10000)
 		},
 		methods: {
+			async rsValue(lines, property, separator, trimmed, lineMatch) {
+				separator = separator || ':'
+				property = property.toLowerCase()
+				trimmed = trimmed || false
+				lineMatch = lineMatch || false
+				for (let i = 0; i < lines.length; i++) {
+					let line = lines[i].toLowerCase().replace(/\t/g, '')
+					if (trimmed) {
+						line = line.trim()
+					}
+					if (line.startsWith(property) && (lineMatch ? line.match(property + separator) : true)) {
+						const parts = trimmed ? lines[i].trim().split(separator) : lines[i].split(separator)
+						if (parts.length >= 2) {
+							parts.shift()
+							return parts.join(separator).trim()
+						} else {
+							return ''
+						}
+					}
+				}
+				return ''
+			},
 			async rsConvert(bytes, decimals = 2) {
 				if (bytes === 0) return '0 Bytes'
 				const k = 1024
@@ -416,6 +446,141 @@
 				const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 				const i = Math.floor(Math.log(bytes) / Math.log(k))
 				return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+			},
+			async compSys() {
+				let r = {}
+				let ps = new PowerShell([
+					'Get-WmiObject win32_computerSystem | Select-Object SystemSKUNumber, Model | fl; Get-WmiObject Win32_computerSystemProduct | select IdentifyingNumber | fl; Get-CimInstance Win32_OperatingSystem | select Caption |fl',
+				])
+
+				// Handle process errors (e.g. powershell not found)
+				ps.on('error', (err) => {
+					console.error(err)
+				})
+
+				// Stdout
+				ps.on('output', (data) => {
+					data = data.toString().split('\r\n')
+					this.form.sku = rsValue(data, 'SystemSKUNumber', ':')
+					this.form.product = rsValue(data, 'Model', ':')
+					this.form.serial = rsValue(data, 'IdentifyingNumber', ':')
+					this.form.winver = rsValue(data, 'Caption', ':')
+					serial = rsValue(data, 'IdentifyingNumber', ':')
+				})
+				ps.on('error-output', (data) => {
+					console.error(data)
+				})
+
+				// End
+				ps.on('end', (code) => {
+					// Do Something on end
+				})
+			},
+			async vdCtrl() {
+				let r = {}
+				// Win32_VideoController
+				let ps = new PowerShell([
+					'Get-WmiObject Win32_VideoController | Select Description, AdapterRAM | ConvertTo-Json',
+				])
+
+				// Handle process errors (e.g. powershell not found)
+				ps.on('error', (err) => {
+					console.error(err)
+				})
+
+				// Stdout
+
+				ps.on('output', async (data) => {
+					let info = await JSON.parse(data)
+					let n = 1
+					console.log('vd: ', Array.isArray(info))
+					if (Array.isArray(info))
+						for (let x in info) {
+							this.form[`gpu${n}`] = `${info[x].Description} ${convert(info[x].AdapterRAM)}`
+							n++
+						}
+					else this.form[`gpu1`] = `${info.Description} ${convert(info.AdapterRAM)}`
+				})
+
+				// Stderr
+				ps.on('error-output', (data) => {
+					console.error(data)
+				})
+
+				// End
+				ps.on('end', (code) => {
+					// return r;
+					// Do Something on end
+				})
+			},
+			async memRam() {
+				let r = {}
+				// Win32_VideoController
+				let ps = new PowerShell([
+					'Get-CimInstance win32_physicalmemory | Select Manufacturer, Capacity, Speed | convertTo-Json',
+				])
+
+				// Handle process errors (e.g. powershell not found)
+				ps.on('error', (err) => {
+					console.error(err)
+				})
+
+				// Stdout
+				ps.on('output', async (data) => {
+					let info = await JSON.parse(data)
+					let n = 1
+					let total = 0
+					console.log('vd: ', Array.isArray(info))
+					if (Array.isArray(info))
+						for (let x in info) {
+							total += info[x].Capacity
+						}
+					else total = info.Capacity
+					this.form[`memoryRam`] = Array.isArray(info)
+						? `${info[0].Manufacturer} ${convert(total)} ${info[0].Speed} GHz`
+						: `${info.Manufacturer} ${convert(total)} ${info.Speed} GHz`
+				})
+				ps.on('error-output', (data) => {
+					console.error(data)
+				})
+
+				// End
+				ps.on('end', (code) => {
+					console.log('finish')
+					// Do Something on end
+				})
+			},
+			async infoCpu() {
+				let r = {}
+				// Win32_VideoController
+				let ps = new PowerShell([
+					'Get-CimInstance Win32_Processor | Select Name, NumberOfCores, CurrentClockSpeed | fl',
+				])
+
+				// Handle process errors (e.g. powershell not found)
+				ps.on('error', (err) => {
+					console.error(err)
+				})
+
+				// Stdout
+				ps.on('output', (data) => {
+					data = data.toString().split('\r\n')
+					this.form.cpu = rsValue(data, 'Name', ':')
+					this.form.cpuSpeed = `${(rsValue(data, 'CurrentClockSpeed', ':') / 1000).toFixed(
+						2
+					)} GHz, ${rsValue(data, 'NumberOfCores', ':')} Cores`
+				})
+				//(rsValue(data,"Capacity",":") / (1000 * 1000 * 1000)).toFixed(2);
+				// Stderr
+				ps.on('error-output', (data) => {
+					console.error(data)
+				})
+
+				// End
+				ps.on('end', (code) => {
+					console.log('finish')
+					// Do Something on end
+				})
 			},
 			async hdd() {
 				return new Promise(async (resolve) => {
@@ -463,6 +628,7 @@
 								}
 							}
 						}
+						resolve('randy')
 					})
 
 					// Stderr
@@ -473,7 +639,6 @@
 					// End
 					ps.on('end', (code) => {
 						// Do Something on end
-						resolve('randy')
 					})
 				})
 			},
@@ -556,6 +721,30 @@
 					})
 				Loading.hide()
 			},
+			async actdWin() {
+				return new Promise(async (resolve) => {
+					let ps = new PowerShell(['slmgr /ato'])
+
+					// Handle process errors (e.g. powershell not found)
+					ps.on('error', (err) => {
+						console.error(err)
+					})
+
+					// Stdout
+					ps.on('output', (data) => {
+						this.license()
+						resolve()
+					})
+					ps.on('error-output', (data) => {
+						console.error(data)
+					})
+
+					// End
+					ps.on('end', (code) => {
+						// Do Something on end
+					})
+				})
+			},
 			async license() {
 				this.loadingL = true
 				process.nextTick(() => {
@@ -579,7 +768,10 @@
 								break
 							}
 						}
-						this.loadingL = false
+						if (!this.numberCheck) {
+							await this.actdWin()
+							this.license()
+						} else this.loadingL = false
 						console.log(this.window)
 					})
 					ps.on('error-output', (data) => {
